14907097f0f1e7e400f7420dba3522906f0a84d1
Connor Allcroft <cjallcroft@hotmail.com>
2022-10-28 14:25:07 -0400

    Updates

---
+++ FSWAD Projects/MEAN Project (Final Code)/UserRegistration.js
@@ -0,0 +1,38 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller("registrationCtrl", function($scope, $http){
+
+    $scope.reset = function() {
+        $scope.userName = "";
+        $scope.password = "";
+        $scope.firstName = "";
+        $scope.lastName = "";
+        $scope.birthDate = "";
+        $scope.email = "";
+        $scope.phone = "";
+    };
+
+    $scope.userRegistration = function(userName, password, firstName, lastName, birthDate, email, phone) {
+        var data = {
+            userName: userName,
+            password: password,
+            firstName: firstName,
+            lastName: lastName,
+            birthDate: birthDate,
+            email: email,
+            phoneNumber: phone
+        };
+
+        var dataPayload = JSON.stringify(data);
+        
+        $http.defaults.headers.post["Content-Type"] = "text/plain";
+
+        $http.post('http://localhost:3000/', dataPayload).then(function(res) {
+            res.end();
+        }, function(res) {
+            res.end();
+        });
+    }
+});
+
+
---
+++ FSWAD Projects/MEAN Project (Final Code)/nodeLogin2.js
@@ -0,0 +1,69 @@
+var http = require('http');
+const express = require('express');
+var fs = require('fs')
+const querystring = require('querystring')
+
+var app = express();
+
+http.createServer(function (req, res) {
+    if (req.url == '/login') {
+
+        var body = "";
+
+        req.on('data', function (chunk) {
+            body += chunk;
+        });
+        req.on('end', function (req) {
+
+            var postData = querystring.decode(body)
+
+            console.log(postData);
+
+            var Mongo = require('mongodb').MongoClient;
+
+            var url = "mongodb+srv://C_Allcroft29:Marauders29%3F@calmongodb.qbyvrif.mongodb.net/?retryWrites=true&w=majority";
+
+            Mongo.connect(url, function(err, db) {
+                if (err) throw err;
+                var dbo = db.db("PlumPicks");
+        
+                var user = {username: postData.uname, password: postData.pword};
+
+                dbo.collection("Users").findOne(user, function(err, result) {
+                    if (err) throw err;
+
+                    if(result==null){
+                        console.log('User not found.');
+                    }
+                    else {
+                        console.log("User found!");
+                        fs.readFile("HomePage.html", function(err, data) {
+                            if (err) {
+                                res.writeHead(500, { "Content-Type": "text/html" });
+                                res.end("<html><head><title>Oops</title></head><body><h1>Oops, there was an error</h1></html>");
+                            }
+                            else {
+                                res.writeHead(200, { "Content-Type": "text/html" });
+                                res.end(data);
+                            }
+                        });
+                        
+                    }
+                });
+
+            });
+        });        
+    }
+    else {
+        fs.readFile("Login.html", function(err, data) {
+            if (err) {
+              res.writeHead(500, { "Content-Type": "text/html" });
+              res.end("<html><head><title>Oops</title></head><body><h1>Oops, there was an error</h1></html>");
+            }
+            else {
+              res.writeHead(200, { "Content-Type": "text/html" });
+              res.end(data);
+            }
+          });
+    }
+}).listen(8000);
